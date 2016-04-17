7a40d07bdceb8f56c824c5b7dadc30c411ecb70c
Ebad Gul <ebadgul66@gmail.com>
2016-04-17 13:01:11 +0100

    improved messaging view

--- app/src/main/java/www/wit/ie/connect/chat/MessageAdapter.java
+++ app/src/main/java/www/wit/ie/connect/chat/MessageAdapter.java
@@ -1,11 +1,6 @@
 package www.wit.ie.connect.chat;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
